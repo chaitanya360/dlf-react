56be4624ad87a4730b9dbde586186ea535d78b11
Chaitanya360 <chaitanyajadhav360@gmail.com>
2021-09-22 19:36:34 +0530

    added model-viewer

--- src/componenets/atoms/Iframe.js
+++ src/componenets/atoms/Iframe.js
@@ -1,17 +1,28 @@
 import React from "react";
-
+import "@google/model-viewer/dist/model-viewer.min.js";
 function Iframe({ src }) {
   return (
-    <iframe
-      class="model"
-      height="100%"
-      width="100%"
-      src={src}
-      frameborder="0"
-      allowFullScreen
-      allow="xr-spatial-tracking; gyroscope; accelerometer"
-      scrolling="no"
-    ></iframe>
+    // <iframe
+    //   class="model"
+    //   height="100%"
+    //   width="100%"
+    //   src={src}
+    //   frameborder="0"
+    //   allowFullScreen
+    //   allow="xr-spatial-tracking; gyroscope; accelerometer"
+    //   scrolling="no"
+    // ></iframe>
+    <model-viewer
+      src={`${process.env.PUBLIC_URL}/models/iso.glb`}
+      alt="A 3D model of an astronaut"
+      auto-rotate
+      ar
+      ar-modes="webxr scene-viewer quick-look"
+      camera-controls
+      style={{ height: "100%", width: "100%" }}
+    >
+      <button slot="ar-button">open ar mode</button>
+    </model-viewer>
   );
 }
 
